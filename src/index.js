001185d57cc49ef2cd53a24ae856f2dfb1e02b9d
Bogdan Nenu <bogdan.nenu@gmail.com>
2017-07-15 19:47:39 +0300

    Fixed export of forms reducer

--- src/index.js
+++ src/index.js
@@ -7,9 +7,14 @@ export { default as pipe } from './packages/pipe'
 export { default as connect } from './packages/connect'
 export { default as either } from './packages/either'
 export { default as nest } from './packages/nest'
+
 import createState, { action } from './packages/stream'
 export { createState, action }
+
 export { default as withDestructured } from './packages/withDestructured'
 export { default as withFunctions } from './packages/withFunctions'
-export { default as withFormState } from './packages/withFormState'
+
+import withFormState,  { forms, formMount, formChange, formSubmit, formError, formUnmount } from './packages/withFormState'
+export { withFormState, forms, formMount, formChange, formSubmit, formError, formUnmount }
+
 export { default as withProp } from './packages/withProp'
